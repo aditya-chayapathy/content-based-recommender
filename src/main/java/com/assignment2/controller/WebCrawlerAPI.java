24d279c607ba39ac997b7384ecf01de99a8cf551
Aditya C <aditya.chayapathy@gmail.com>
2018-10-12 21:06:54 -0700

    adding code to store data in persistent lucene index

--- src/main/java/com/assignment2/controller/WebCrawlerAPI.java
+++ src/main/java/com/assignment2/controller/WebCrawlerAPI.java
@@ -4,7 +4,10 @@ import com.assignment2.services.WebCrawlerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/webCrawler")
@@ -14,7 +17,14 @@ public class WebCrawlerAPI {
     WebCrawlerService webCrawlerService;
 
     @RequestMapping(value = "/crawlJavaWikibooks", method = RequestMethod.POST)
-    public Boolean crawlJavaWikibooks() {
+    public Boolean crawlJavaWikibooks() throws Exception {
         return webCrawlerService.crawlJavaWikibooks();
     }
+
+    @RequestMapping(value = "/queryIndex", method = RequestMethod.GET)
+    public List<String> queryIndex(@RequestParam(name = "content") String content,
+                                   @RequestParam(name = "hits") Integer hits) throws Exception {
+        return webCrawlerService.queryIndex(content, hits);
+    }
+
 }
--- src/main/java/com/assignment2/services/WebCrawlerService.java
+++ src/main/java/com/assignment2/services/WebCrawlerService.java
@@ -1,5 +1,18 @@
 package com.assignment2.services;
 
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopScoreDocCollector;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -7,6 +20,7 @@ import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -14,40 +28,83 @@ import java.util.List;
 @Service
 public class WebCrawlerService {
 
-    public Boolean crawlJavaWikibooks() {
+    public Boolean crawlJavaWikibooks() throws Exception {
         String uri = "https://en.wikibooks.org/wiki/Java_Programming";
         Integer maxDepth = 2;
         HashSet<String> urls = new HashSet<>();
 
         recursiveJavaWordbooksCrawling(uri, urls, 1, maxDepth);
 
-        List<String> docs = new ArrayList<>();
+        Directory indexDir = FSDirectory.open(Paths.get("./index"));
+        StandardAnalyzer analyzer = new StandardAnalyzer();
+        IndexWriterConfig config = new IndexWriterConfig(analyzer);
+        IndexWriter writer = new IndexWriter(indexDir, config);
+
         for (String url : urls) {
             try {
                 Document document = Jsoup.connect(url).get();
                 Element div = document.getElementsByClass("mw-parser-output").get(0);
                 Elements paragraphs = div.getElementsByTag("p");
                 for (Element paragraph : paragraphs) {
-                    docs.add(paragraph.text());
+                    org.apache.lucene.document.Document doc = new org.apache.lucene.document.Document();
+                    doc.add(new TextField("url", url, TextField.Store.YES));
+                    doc.add(new TextField("type", "paragraph", TextField.Store.YES));
+                    doc.add(new TextField("content", paragraph.text(), TextField.Store.YES));
+                    writer.addDocument(doc);
                 }
                 Elements tables = div.getElementsByTag("table");
                 for (Element table : tables) {
-                    docs.add(table.text());
+                    org.apache.lucene.document.Document doc = new org.apache.lucene.document.Document();
+                    doc.add(new TextField("url", url, TextField.Store.YES));
+                    doc.add(new TextField("type", "table", TextField.Store.YES));
+                    doc.add(new TextField("content", table.text(), TextField.Store.YES));
+                    writer.addDocument(doc);
                 }
                 Elements unorderedLists = div.getElementsByTag("ul");
                 for (Element unorderedList : unorderedLists) {
-                    docs.add(unorderedList.text());
+                    org.apache.lucene.document.Document doc = new org.apache.lucene.document.Document();
+                    doc.add(new TextField("url", url, TextField.Store.YES));
+                    doc.add(new TextField("type", "unorderedList", TextField.Store.YES));
+                    doc.add(new TextField("content", unorderedList.text(), TextField.Store.YES));
+                    writer.addDocument(doc);
                 }
                 Elements orderedLists = div.getElementsByTag("ol");
                 for (Element orderedList : orderedLists) {
-                    docs.add(orderedList.text());
+                    org.apache.lucene.document.Document doc = new org.apache.lucene.document.Document();
+                    doc.add(new TextField("url", url, TextField.Store.YES));
+                    doc.add(new TextField("type", "orderedList", TextField.Store.YES));
+                    doc.add(new TextField("content", orderedList.text(), TextField.Store.YES));
+                    writer.addDocument(doc);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+        writer.close();
 
         return Boolean.TRUE;
+    }
+
+    public List<String> queryIndex(String content, Integer hitsPerPage) throws Exception {
+        Directory indexDir = FSDirectory.open(Paths.get("./index"));
+        StandardAnalyzer analyzer = new StandardAnalyzer();
+
+        Query q = new QueryParser("content", analyzer).parse(content);
+        IndexReader reader = DirectoryReader.open(indexDir);
+        TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage);
+        IndexSearcher searcher = new IndexSearcher(reader);
+        searcher.search(q, collector);
+        ScoreDoc[] hits = collector.topDocs().scoreDocs;
+
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < hits.length; ++i) {
+            int docId = hits[i].doc;
+            org.apache.lucene.document.Document d = searcher.doc(docId);
+            result.add(d.get("content"));
+        }
+        reader.close();
+
+        return result;
     }
 
     private void recursiveJavaWordbooksCrawling(String startURI, HashSet<String> urls, Integer currentDepth, Integer maxDepth) {
